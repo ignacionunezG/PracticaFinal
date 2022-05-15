603a594b4b59eb7a89a71806bfe8c6080bac4de2
ignacionunezG <201900700@alu.comillas.edu>
2022-05-15 11:43:03 +0200

    model

--- madPark/src/main/java/com/PracticaFinal/madPark/model/Customer.java
+++ madPark/src/main/java/com/PracticaFinal/madPark/model/Customer.java
@@ -19,8 +19,48 @@ public class Customer {
     private int cvv;
     private String caducidad;
 
-    public void setId(String email) {
+    public void setName(String name) {
+        this.name=name;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos=apellidos;
+    }
+    public void setEmail(String email) {
         this.email=email;
     }
-    
+    public void setPassword(String password) {
+        this.password=password;
+    }
+    public void setNumTarjeta(Long numTarjeta) {
+        this.numTarjeta=numTarjeta;
+    }
+    public void setCvv(int cvv) {
+        this.cvv=cvv;
+    }
+    public void setCaducidad(String caducidad) {
+        this.caducidad=caducidad;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getApellidos(){
+        return this.apellidos;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getPasswoord(){
+        return this.password;
+    }
+    public Long getNumTarjeta(){
+        return this.numTarjeta;
+    }
+    public int getCvv(){
+        return this.cvv;
+    }
+    public String getCaducidad(){
+        return this.caducidad;
+    }
 }
--- madPark/src/main/java/com/PracticaFinal/madPark/model/Matriculas.java
+++ madPark/src/main/java/com/PracticaFinal/madPark/model/Matriculas.java
@@ -12,12 +12,51 @@ import lombok.Data;
 @Builder
 @Table("MATRICULAS")
 public class Matriculas {
-    
+    @Id
     private String email;
     private String matricula1;
     private String matricula2;
     private String matricula3;
     private String matricula4;
     private String matricula5;
+
+    public void setEmail(String email) {
+        this.email=email;
+    }
+    public void setMatricula1(String matricula1) {
+        this.matricula1=matricula1;
+    }
+    public void setMatricula2(String matricula2) {
+        this.matricula2=matricula2;
+    }
+    public void setMatricula3(String matricula3) {
+        this.matricula3=matricula3;
+    }
+    public void setMatricula4(String matricula4) {
+        this.matricula4=matricula4;
+    }
+    public void setMatricula5(String matricula5) {
+        this.matricula5=matricula5;
+    }
+
+    
+    public String getEmail(){
+        return this.email;
+    }
+    public String getMatricula1(){
+        return this.matricula1;
+    }
+    public String getMatricula2(){
+        return this.matricula2;
+    }
+    public String getMatricula3(){
+        return this.matricula3;
+    }
+    public String getMatricula4(){
+        return this.matricula4;
+    }
+    public String getMatricula5(){
+        return this.matricula5;
+    }
   
 }
