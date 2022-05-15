59cc4c397f6ded75a341e9e6d7921f4f8e5e8d9e
ignacionunezG <201900700@alu.comillas.edu>
2022-05-15 12:10:18 +0200

    models

--- madPark/src/main/java/com/PracticaFinal/madPark/model/Historial.java
+++ madPark/src/main/java/com/PracticaFinal/madPark/model/Historial.java
@@ -9,9 +9,32 @@ import lombok.Data;
 @Builder
 @Table("HISTORIAL")
 public class Historial {
- 
+
     private String email;
     private int idPark;
     private String parking;
-    
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public int getIdPark() {
+        return this.idPark;
+    }
+
+    public String getParking() {
+        return this.parking;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setIdPark(int idPark) {
+        this.idPark = idPark;
+    }
+
+    public void setParking(String parking) {
+        this.parking = parking;
+    }
 }
