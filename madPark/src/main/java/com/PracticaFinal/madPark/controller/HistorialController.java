871e3fbd293fc9a3c2cf4fdcca290b1248aa35e8
alejandrovalbuena <alejandro01.av@gmail.com>
2022-05-20 19:05:41 +0200

    about us

--- madPark/src/main/java/com/PracticaFinal/madPark/controller/HistorialController.java
+++ madPark/src/main/java/com/PracticaFinal/madPark/controller/HistorialController.java
@@ -1,15 +1,21 @@
 package com.PracticaFinal.madPark.controller;
 
+import java.util.List;
+
 import com.PracticaFinal.madPark.model.Historial;
+import com.PracticaFinal.madPark.repository.HistorialRepository;
 import com.PracticaFinal.madPark.service.HistorialService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,20 +25,22 @@ public class HistorialController {
     public record DataResponse (String result) {}
 
     @Autowired
-    private HistorialService HistorialService;
+    private HistorialService service;
+
+    @Autowired
+    private HistorialRepository repository;
 
     
     @GetMapping("/historial")
     public ResponseEntity<Iterable<Historial>> retrieveAllHistorial() {
-        Iterable<Historial> response = HistorialService.retrieveAllHistorial();
-        return ResponseEntity.ok().body(response);
-
+        return ResponseEntity.ok().body(service.retrieveAllHistorial());
     }
+    
 
 
     @GetMapping("/historial/{email}/")
     public ResponseEntity<DataResponse> retrieveHistorial(@PathVariable String email) {
-        Historial response = HistorialService.retrieveHistorial(email);
+        Historial response = service.retrieveHistorial(email);
 
         if (response == null) {
             return ResponseEntity.ok().body(new DataResponse("KO"));
@@ -45,7 +53,7 @@ public class HistorialController {
 
     @PutMapping("/historial/{email}/")
     public ResponseEntity<Historial> updateHistorial(@PathVariable String email, @RequestBody Historial Historial) {
-        Historial newHistorial = HistorialService.updateHistorial(email, Historial);
+        Historial newHistorial = service.updateHistorial(email, Historial);
         if (newHistorial == null) {
             return ResponseEntity.badRequest().body(null);
         }
